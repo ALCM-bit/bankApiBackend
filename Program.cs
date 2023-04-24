277e48564dd1ee91775f76e9d9a6b4df0bf21150
Felipe Augusto L. C. Magalhães <felipealcmagalhaes@gmail.com>
2023-04-24 10:27:25 -0300

    Ajust

--- Program.cs
+++ Program.cs
@@ -141,5 +141,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<BankContext>();
+    context.Database.Migrate();
+}
 
 app.Run();
