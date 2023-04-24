ad44c39cfedfc9d53573e1541fdcc8118eae48f5
Felipe Augusto L. C. Magalhães <felipealcmagalhaes@gmail.com>
2023-04-24 10:23:26 -0300

    Test with no auto migrate

--- Program.cs
+++ Program.cs
@@ -141,11 +141,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<BankContext>();
-    context.Database.Migrate();
-}
 
 app.Run();
